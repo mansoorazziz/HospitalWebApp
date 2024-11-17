c3bf42addc547a8a553500009ceb449a270b6dd5
mansooraziz <mansoorpay@gmail.com>
2024-11-17 10:45:56 +0500

    Doctors portion Completed

--- doctors/urls.py
+++ doctors/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add/', views.add_doctor, name='add_doctor'),
-    path('', views.doctor_list, name='doctor_list'),  # Assuming you have a list view
+    path('add/', views.add_doctor, name='add_doctor'), # Add Doctor
+    path('', views.doctor_list, name='doctor_list'),  # list view Doctor
+    path('delete/<int:serial_no>/',views.delete_doctor, name='delete_doctor'), # Delete Doctor
+    path('edit/<int:serial_no>/',views.edit_doctor, name='edit_doctor'), # Update Doctor
 ]
--- doctors/views.py
+++ doctors/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import DoctorForm
 from .models import Doctor
 # from django.contrib import messages
+
 # Create your views here.
 
 # Adding new Doctors
@@ -23,3 +24,24 @@ def add_doctor(request):
 def doctor_list(request):
     doctors = Doctor.objects.all()
     return render(request, 'doctors/doctor_list.html', {'doctors': doctors})
+
+# Doctor deletion
+def delete_doctor(request, serial_no):
+    doctor = get_object_or_404(Doctor, serial_no=serial_no)
+    if request.method == 'POST':
+        doctor.delete()
+        return redirect('doctor_list')
+    return render(request, 'doctors/delete_doctor.html', {'doctor': doctor})
+
+# Update Doctor
+def edit_doctor(request, serial_no):
+    doctor = get_object_or_404(Doctor, serial_no=serial_no)
+    if request.method == 'POST':
+        form = DoctorForm(request.POST, instance=doctor)
+        if form.is_valid():
+            form.save()
+            return redirect('doctor_list')
+    else:
+        form = DoctorForm(instance=doctor)
+    return render(request, 'doctors/edit_doctor.html', {'form': form, 'doctor': doctor})
+
