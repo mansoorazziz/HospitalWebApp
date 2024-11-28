6a39e1c9ea25f7d7acf15ef45b20ec244f1dbe5f
mansooraziz <mansoorpay@gmail.com>
2024-11-28 20:47:39 +0500

    one step more

--- appointments/views.py
+++ appointments/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import AppointmentForm
-from .models import Appointment
+from .models import Appointment,Doctor,Patient
 
 # Add new appointment
 def add_appointment(request):
     if request.method == 'POST':
         form = AppointmentForm(request.POST)
+        print(request.POST) # Debug: Print POST data
         if form.is_valid():
             form.save()
             return redirect('appointment_list')
+        else: 
+            print(form.errors) # Debug: Print form errors
     else:
         form = AppointmentForm()
     return render(request, 'appointments/add_appointment.html', {'form': form})
@@ -16,7 +19,14 @@ def add_appointment(request):
 # Appointments List
 def appointment_list(request):
     appointments = Appointment.objects.all()
-    return render(request, 'appointments/appointment_list.html', {'appointments': appointments})
+    doctors = Doctor.objects.all()
+    patients = Patient.objects.all()
+
+    return render(request, 'appointments/appointment_list.html', {
+        'appointments': appointments,
+        'doctors': doctors,
+        'patients': patients
+    })
 
 # Update appointment
 def edit_appointment(request, serial_no):
